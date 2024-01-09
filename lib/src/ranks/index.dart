affeef0abddf171bb2aaca7b3a4352db4a81f60c
orenagiv <oa@pnc.co.il>
2024-01-09 16:09:55 +0200

    Fixed index.dart export

--- lib/src/ranks/index.dart
+++ lib/src/ranks/index.dart
@@ -1,4 +1,4 @@
 export 'cl100k_base.tiktoken.dart';
-// export 'gpt2.tiktoken.dart';
-// export 'p50k_base.tiktoken.dart';
-// export 'r50k_base.tiktoken.dart';
+export 'gpt2.tiktoken.dart';
+export 'p50k_base.tiktoken.dart';
+export 'r50k_base.tiktoken.dart';
