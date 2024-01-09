ff8b570629f813811ef989c3daa0a4e277b8373e
orenagiv <oa@pnc.co.il>
2024-01-09 17:09:27 +0200

    Converted to parts

--- lib/src/core_bpe_constructor.dart
+++ lib/src/core_bpe_constructor.dart
@@ -4,6 +4,18 @@ import 'package:langchain_tiktoken/src/common/byte_array.dart';
 
 import 'ranks/index.dart' as ranks;
 
+import 'ranks/cl100k_base/cl100k_base_1.g.dart';
+import 'ranks/cl100k_base/cl100k_base_2.g.dart';
+import 'ranks/cl100k_base/cl100k_base_3.g.dart';
+import 'ranks/cl100k_base/cl100k_base_4.g.dart';
+import 'ranks/cl100k_base/cl100k_base_5.g.dart';
+import 'ranks/cl100k_base/cl100k_base_6.g.dart';
+import 'ranks/cl100k_base/cl100k_base_7.g.dart';
+import 'ranks/cl100k_base/cl100k_base_8.g.dart';
+import 'ranks/cl100k_base/cl100k_base_9.g.dart';
+import 'ranks/cl100k_base/cl100k_base_10.g.dart';
+import 'ranks/cl100k_base/cl100k_base_11.g.dart';
+
 // ignore: constant_identifier_names
 const ENDOFTEXT = "<|endoftext|>";
 
@@ -28,63 +40,75 @@ class CoreBPEConstructor {
     this.explicitNVocab,
   });
 
-  factory CoreBPEConstructor.gpt2() {
-    return CoreBPEConstructor._(
-      name: "gpt2",
-      explicitNVocab: 50257,
-      patStr:
-          r"'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+",
-      mergeableRanks: ranks.gpt2.map(
-        (k, v) => MapEntry(ByteArray.fromList(base64Decode(k)), v),
-      ),
-      specialTokens: {ENDOFTEXT: 50256},
-    );
-  }
+  // factory CoreBPEConstructor.gpt2() {
+  //   return CoreBPEConstructor._(
+  //     name: "gpt2",
+  //     explicitNVocab: 50257,
+  //     patStr:
+  //         r"'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+",
+  //     mergeableRanks: ranks.gpt2.map(
+  //       (k, v) => MapEntry(ByteArray.fromList(base64Decode(k)), v),
+  //     ),
+  //     specialTokens: {ENDOFTEXT: 50256},
+  //   );
+  // }
 
-  factory CoreBPEConstructor.r50kBase() {
-    return CoreBPEConstructor._(
-      name: "r50k_base",
-      explicitNVocab: 50257,
-      patStr:
-          r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""",
-      mergeableRanks: ranks.r50kBase.map(
-        (k, v) => MapEntry(ByteArray.fromList(base64Decode(k)), v),
-      ),
-      specialTokens: {ENDOFTEXT: 50256},
-    );
-  }
+  // factory CoreBPEConstructor.r50kBase() {
+  //   return CoreBPEConstructor._(
+  //     name: "r50k_base",
+  //     explicitNVocab: 50257,
+  //     patStr:
+  //         r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""",
+  //     mergeableRanks: ranks.r50kBase.map(
+  //       (k, v) => MapEntry(ByteArray.fromList(base64Decode(k)), v),
+  //     ),
+  //     specialTokens: {ENDOFTEXT: 50256},
+  //   );
+  // }
 
-  factory CoreBPEConstructor.p50kBase() {
-    return CoreBPEConstructor._(
-      name: "p50k_base",
-      explicitNVocab: 50281,
-      patStr:
-          r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""",
-      mergeableRanks: ranks.p50kBase.map(
-        (k, v) => MapEntry(ByteArray.fromList(base64Decode(k)), v),
-      ),
-      specialTokens: {ENDOFTEXT: 50256},
-    );
-  }
+  // factory CoreBPEConstructor.p50kBase() {
+  //   return CoreBPEConstructor._(
+  //     name: "p50k_base",
+  //     explicitNVocab: 50281,
+  //     patStr:
+  //         r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""",
+  //     mergeableRanks: ranks.p50kBase.map(
+  //       (k, v) => MapEntry(ByteArray.fromList(base64Decode(k)), v),
+  //     ),
+  //     specialTokens: {ENDOFTEXT: 50256},
+  //   );
+  // }
 
-  factory CoreBPEConstructor.p50kEdit() {
-    return CoreBPEConstructor._(
-      name: "p50k_edit",
-      patStr:
-          r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""",
-      mergeableRanks: ranks.p50kBase.map(
-        (k, v) => MapEntry(ByteArray.fromList(base64Decode(k)), v),
-      ),
-      specialTokens: {
-        ENDOFTEXT: 50256,
-        FIM_PREFIX: 50281,
-        FIM_MIDDLE: 50282,
-        FIM_SUFFIX: 50283
-      },
-    );
-  }
+  // factory CoreBPEConstructor.p50kEdit() {
+  //   return CoreBPEConstructor._(
+  //     name: "p50k_edit",
+  //     patStr:
+  //         r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""",
+  //     mergeableRanks: ranks.p50kBase.map(
+  //       (k, v) => MapEntry(ByteArray.fromList(base64Decode(k)), v),
+  //     ),
+  //     specialTokens: {
+  //       ENDOFTEXT: 50256,
+  //       FIM_PREFIX: 50281,
+  //       FIM_MIDDLE: 50282,
+  //       FIM_SUFFIX: 50283
+  //     },
+  //   );
+  // }
 
   factory CoreBPEConstructor.cl100kBase() {
+    Map<String, int> cl100kBase = cl100kBase1;
+    cl100kBase.addAll(cl100kBase2);
+    cl100kBase.addAll(cl100kBase3);
+    cl100kBase.addAll(cl100kBase4);
+    cl100kBase.addAll(cl100kBase5);
+    cl100kBase.addAll(cl100kBase6);
+    cl100kBase.addAll(cl100kBase7);
+    cl100kBase.addAll(cl100kBase8);
+    cl100kBase.addAll(cl100kBase9);
+    cl100kBase.addAll(cl100kBase10);
+    cl100kBase.addAll(cl100kBase11);
+
     return CoreBPEConstructor._(
       name: "cl100k_base",
       patStr:
